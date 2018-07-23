4fa6b04adad25f1f419b03bfd69908cd2622dceb
milosvujinovic <vujinovic.milos@gmail.com>
2018-07-23 20:33:27 +0200

    Initial commit

---
+++ admin/includes/admin_content.php
@@ -0,0 +1,95 @@
+<div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Blank Page
+                            <small>Subheading</small>
+                        </h1>
+
+
+
+                    <?php 
+// $photo = Photo::find_by_id();
+// echo $photo->filename;
+//                                 // $user = new User();
+                                // $result_set = User::find_all_users();
+                                // while($row = mysqli_fetch_array($result_set)){
+                                // echo "<div style='border: 1px solid red;'>" . $row['username'] . "</div>";
+                                // echo "</br>";
+                                
+                                // }
+
+                            // $found_user = User::find_user_by_id(2);
+                            // $user = User::instantation($found_user);
+                                
+                            //     echo $user->username;
+
+                                // $found_user = User::find_user_by_id(3);
+                                // echo $found_user->username;
+
+
+
+
+// $user = new User();
+// $user->username = "anaaaaa";
+// $user->password = "123";
+// $user->first_name = "un";
+// $user->last_name = "aaaaaaaaaa";
+// $user->create();
+
+// $user = User::find_user_by_id(4);
+// $user->last_name = "Miloooooooooo";
+// $user->update();
+
+
+//$user = User::find_user_by_id(1);
+//$user->delete();
+
+
+// $user = User::find_user_by_id(16);
+// $user->username = "sdgasvs";
+// $user->save();
+
+// $user = new User();
+// $user->username = "Misooooooo";
+// $user->save();
+// 
+// 
+// 
+
+                            // $users = User::find_all();
+                            // foreach($users as $user) {
+                            //     echo $user->username;
+                            // }
+                            
+                            
+                            //   $photos = Photo::find_all();
+                            // foreach($photos as $photo) {
+                            //     echo $photo->title;
+                            // }
+                            
+
+                           //  $photo = new Photo();
+                           //  $photo->title = "Photo from ";
+                           //  $photo->description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book";
+                           //  $photo->filename = "image1.jpg";
+                           // $photo->type = "image";
+                           //  $photo->size = 11;
+                           //  $photo->create();
+
+
+
+
+                    ?>
+
+                        <ol class="breadcrumb">
+                            <li>
+                                <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
+                            </li>
+                            <li class="active">
+                                <i class="fa fa-file"></i> Blank Page
+                            </li>
+                        </ol>
+                    </div>
